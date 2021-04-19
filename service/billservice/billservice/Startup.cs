8e295113ca803d9befaf4b0e57a113c9829ae162
aierong <aierong@126.com>
2021-04-19 17:13:41 +0800

    s

--- service/billservice/billservice/Startup.cs
+++ service/billservice/billservice/Startup.cs
@@ -28,6 +28,13 @@ namespace billservice
         public void ConfigureServices ( IServiceCollection services )
         {
             services.AddControllers();
+
+            //.net core3.1中返回数据属性字段首字母是默认小写(驼峰格式)
+            services.AddMvc().AddJsonOptions( ( options ) =>
+            {
+                // 该值指定用于将对象的属性名称转换为其他格式（例如 camel 大小写）的策略；若为 null，则保持属性名称不变。
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            } );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
