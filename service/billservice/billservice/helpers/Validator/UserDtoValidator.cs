f1feba533aa251718bf1de191269c33b95df1ada
aierong <aierong@126.com>
2021-04-19 17:53:05 +0800

    company

---
+++ service/billservice/billservice/helpers/Validator/UserDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using billservice.models.Dto;
+using FluentValidation;
+
+namespace billservice.helpers.Validator
+{
+    public class UserDtoValidator : AbstractValidator<UserDto>
+    {
+
+
+    }
+}
