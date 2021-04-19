55c016b6f5f2119ff0872c0ace2d7301053c0920
aierong <aierong@126.com>
2021-04-19 17:24:03 +0800

    0

---
+++ service/billservice/billservice/models/billtype.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace billservice.models
+{
+    ///<summary>
+    ///
+    ///</summary>
+    [SugarTable("billtype")]
+    public partial class billtype
+    {
+           public billtype(){
+
+
+           }
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           [SugarColumn(IsPrimaryKey=true,IsIdentity=true)]
+           public int ids {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:0
+           /// Nullable:False
+           /// </summary>           
+           public bool isout {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:1
+           /// Nullable:False
+           /// </summary>           
+           public bool issystemtype {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string typename {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string avatar {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string mobile {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:DateTime.Now
+           /// Nullable:False
+           /// </summary>           
+           public DateTime adddate {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public DateTime? updatedate {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public DateTime? deletedate {get;set;}
+
+    }
+}
---
+++ service/billservice/billservice/models/users.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace billservice.models
+{
+    ///<summary>
+    ///
+    ///</summary>
+    [SugarTable("users")]
+    public partial class users
+    {
+           public users(){
+
+
+           }
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           [SugarColumn(IsIdentity=true)]
+           public int ids {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:user
+           /// Nullable:False
+           /// </summary>           
+           public string rolename {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           [SugarColumn(IsPrimaryKey=true)]
+           public string mobile {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string avatar {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string password {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string name {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string email {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public DateTime? lastlogindate {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:0
+           /// Nullable:False
+           /// </summary>           
+           public int logintimes {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:DateTime.Now
+           /// Nullable:False
+           /// </summary>           
+           public DateTime adddate {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public DateTime? updatedate {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public DateTime? deletedate {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:N
+           /// Nullable:False
+           /// </summary>           
+           public string delmark {get;set;}
+
+    }
+}
