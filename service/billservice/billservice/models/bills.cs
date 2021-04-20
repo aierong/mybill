21fca89f2f8c9d6d48255bead4afed1f77b041e4
aierong <aierong@126.com>
2021-04-20 15:34:17 +0800

    1

--- service/billservice/billservice/models/bills.cs
+++ service/billservice/billservice/models/bills.cs
@@ -8,97 +8,130 @@ namespace billservice.Models
     ///<summary>
     ///
     ///</summary>
-    [SugarTable("bills")]
+    [SugarTable( "bills" )]
     public partial class bills
     {
-           public bills(){
 
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        [SugarColumn( IsPrimaryKey = true , IsIdentity = true )]
+        public int ids
+        {
+            get; set;
+        }
 
-           }
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:False
-           /// </summary>           
-           [SugarColumn(IsPrimaryKey=true,IsIdentity=true)]
-           public int ids {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        public string mobile
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:False
-           /// </summary>           
-           public string mobile {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:0
+        /// Nullable:False
+        /// </summary>           
+        public int billtypeid
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:0
-           /// Nullable:False
-           /// </summary>           
-           public int billtypeid {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:0
+        /// Nullable:False
+        /// </summary>           
+        public bool isout
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:0
-           /// Nullable:False
-           /// </summary>           
-           public bool isout {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:0
+        /// Nullable:False
+        /// </summary>           
+        public decimal moneys
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:0
-           /// Nullable:False
-           /// </summary>           
-           public decimal moneys {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        public string moneydate
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:False
-           /// </summary>           
-           public string moneydate {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public string memo
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public string memo {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public string sources
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public string sources {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:DateTime.Now
+        /// Nullable:False
+        /// </summary>           
+        public DateTime adddate
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:DateTime.Now
-           /// Nullable:False
-           /// </summary>           
-           public DateTime adddate {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public DateTime? updatedate
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public DateTime? updatedate {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public DateTime? deletedate
+        {
+            get; set;
+        }
 
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:True
-           /// </summary>           
-           public DateTime? deletedate {get;set;}
-
-           /// <summary>
-           /// Desc:
-           /// Default:N
-           /// Nullable:False
-           /// </summary>           
-           public string delmark {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:N
+        /// Nullable:False
+        /// </summary>           
+        public string delmark
+        {
+            get; set;
+        }
 
     }
 }
--- service/billservice/billservice/models/roles.cs
+++ service/billservice/billservice/models/roles.cs
@@ -8,28 +8,31 @@ namespace billservice.Models
     ///<summary>
     ///
     ///</summary>
-    [SugarTable("roles")]
+    [SugarTable( "roles" )]
     public partial class roles
     {
-           public roles(){
 
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        [SugarColumn( IsIdentity = true )]
+        public int ids
+        {
+            get; set;
+        }
 
-           }
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:False
-           /// </summary>           
-           [SugarColumn(IsIdentity=true)]
-           public int ids {get;set;}
-
-           /// <summary>
-           /// Desc:
-           /// Default:
-           /// Nullable:False
-           /// </summary>           
-           [SugarColumn(IsPrimaryKey=true)]
-           public string name {get;set;}
+        /// <summary>
+        /// Desc:
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        [SugarColumn( IsPrimaryKey = true )]
+        public string name
+        {
+            get; set;
+        }
 
     }
 }
