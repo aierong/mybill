3aaff8f7323a2711ebc4d7b62b9dfc86ed6e9749
aierong <aierong@126.com>
2021-04-20 17:19:03 +0800

    0

--- service/billservice/billservice/Helpers/Validator/UserDtoValidator.cs
+++ service/billservice/billservice/Helpers/Validator/UserDtoValidator.cs
@@ -11,7 +11,27 @@ namespace billservice.Helpers.Validator
     {
 
 
+        public UserDtoValidator ()
+        {
+            RuleFor( user => user.mobile )
+                .NotNull().WithMessage( "手机号码没有传递或者空" )
+                .NotEmpty().WithMessage( "手机号码为空" );
 
+
+            RuleFor( user => user.password )
+                .NotNull().WithMessage( "密码没有传递或者空" )
+                .NotEmpty().WithMessage( "密码为空" );
+
+
+            RuleFor( user => user.name )
+                .NotNull().WithMessage( "姓名没有传递或者空" )
+                .NotEmpty().WithMessage( "姓名为空" );
+
+            RuleFor( user => user.avatar )
+               .NotNull().WithMessage( "头像没有传递或者空" )
+               .NotEmpty().WithMessage( "头像为空" );
+
+        }
 
     }
 }
