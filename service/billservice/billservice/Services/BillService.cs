5975f6acb5fa4e79d65b541a9abcc17f1dec6aa6
aierong <aierong@126.com>
2021-04-21 20:54:19 +0800

    0

--- service/billservice/billservice/Services/BillService.cs
+++ service/billservice/billservice/Services/BillService.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using billservice.Services.Interfaces;
 
 namespace billservice.Services
 {
-    public class Class
+    public class BillService : IBill
     {
     }
 }
