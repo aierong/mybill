96506646f2ffcb087fa6cf079bad7033ac0f98de
aierong <aierong@126.com>
2021-04-22 17:15:20 +0800

    company

--- service/billservice/billservice/Helpers/Profile/BillTypeProfile.cs
+++ service/billservice/billservice/Helpers/Profile/BillTypeProfile.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using billservice.Models;
+using billservice.Models.Dto;
 
 namespace billservice.Helpers.Profile
 {
-    public class BillTypeProfile
+    public class BillTypeProfile : AutoMapper.Profile
     {
+        public BillTypeProfile ()
+        {
+            CreateMap<BillTypeDto , billtype>()
+                //.ForMember( destination => destination.password , opt => opt.MapFrom( src => new MD5Security().Encrypt( src.password ) ) )
+               .AfterMap( ( src , dest ) =>
+               {
+                   DateTime now = DateTime.Now;
+
+                   dest.issystemtype = false;
+
+                   dest.avatar = ""; // 用户加的类型,有一个固定头像
+                                      
+
+                   dest.adddate = now;
+                   dest.updatedate = null;
+                   dest.deletedate = null;
+
+                    
+               } );
+
+
+
+        }
     }
 }
