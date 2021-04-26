b4faa1f34e829efac31e7818319dd2a44f59ae69
aierong <aierong@126.com>
2021-04-26 14:24:44 +0800

    1

--- service/billservice/billservice/Controllers/BillTypesController.cs
+++ service/billservice/billservice/Controllers/BillTypesController.cs
@@ -10,7 +10,7 @@ using billservice.models.Dto;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 
 namespace billservice.Controllers
 {
@@ -19,7 +19,7 @@ namespace billservice.Controllers
     [ApiController]
     public class BillTypesController : Base.BaseController
     {
-        private readonly IMapper mapper;
+        readonly IMapper mapper;
         readonly IBillType Ibilltype;
 
         public BillTypesController ( IBillType Ibilltype , IMapper mapper )
@@ -30,13 +30,12 @@ namespace billservice.Controllers
 
 
 
-        // POST api/<BillTypeController>
+        
         [HttpPost]
         public ServiceResult add ( [FromBody] BillTypeDto billTypeDto )
         {
             var result = new ServiceResult();
-            //base.HttpContext.User
-
+            
             billtype _billtype = this.mapper.Map<BillTypeDto , billtype>( billTypeDto );
 
             if ( _billtype != null )
--- service/billservice/billservice/Controllers/UsersController.cs
+++ service/billservice/billservice/Controllers/UsersController.cs
@@ -44,6 +44,7 @@ namespace billservice.Controllers
         }
 
 
+
         [AllowAnonymous]
         [HttpPost]
         [Route( "login" )]
