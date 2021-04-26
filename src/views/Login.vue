32f5c95b636ba13e32a4103770f3feca2b6e3108
aierong <aierong@126.com>
2021-04-26 16:40:50 +0800

    company

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<!--
+
+作者: chenghao
+Date: 2021/4/26
+Time: 16:40
+功能: TypeScript版本vue文件
+
+-->
+
+<!-- html代码片段 -->
+<template>
+
+    <div>
+
+    </div>
+
+</template>
+
+<!-- TypeScript脚本代码片段 -->
+<script lang="ts">
+
+// 导入
+import {
+    defineComponent ,
+
+    ref ,
+    reactive ,
+    toRefs ,
+    computed
+} from "vue";
+
+export default defineComponent( {
+    // 子组件
+    components : {} ,
+    // 声明 props
+    props : {} ,
+    setup () {
+
+        return {};
+    } ,
+
+} )
+</script>
+
+<!-- 样式代码片段  scoped -->
+<style>
+
+</style>
