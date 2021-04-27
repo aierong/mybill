b70096d90d632dadcd58c14d0d45c4033a8322d7
aierong <aierong@126.com>
2021-04-27 18:22:51 +0800

    0

---
+++ service/billservice/billservice/Helpers/ConfigData/CorsConfigData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace billservice.Helpers.ConfigData
+{
+    public class CorsConfigData
+    {
+        public string name
+        {
+            get; set;
+        }
+
+
+        public List<string> urls
+        {
+            get; set;
+        }
+    }
+}
