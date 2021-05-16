d40f77d74e0ff0fa3db1b32e8b134508a3aaf067
aierong <aierong@126.com>
2021-05-16 18:36:30 +0800

    home

--- vue.config.js
+++ vue.config.js
@@ -8,23 +8,37 @@ function resolve ( dir ) {
     return path.join( __dirname , dir )
 }
 
+function addStyleResource ( rule ) {
+    rule.use( 'style-resource' )
+    .loader( 'style-resources-loader' )
+    .options( {
+        patterns : [
+            path.resolve( __dirname , './src/styles/imports.less' ) ,
+        ] ,
+    } )
+}
+
 module.exports = {
     //关闭eslint
     lintOnSave : false ,
 
-    chainWebpack : ( config ) => {
+    chainWebpack : config => {
         //vue有一个默认的别名:@ 对应 src
         //下面定义了2个别名
-        config.resolve.alias
         // 链式，接着可以往下写
-        .set( '@comp' , resolve( 'src/components' ) )
+        config.resolve.alias.set( '@comp' , resolve( 'src/components' ) )
         .set( '@views' , resolve( 'src/views' ) )
         .set( '@common' , resolve( 'src/common' ) )
         .set( '@https' , resolve( 'src/https' ) )
         .set( '@store' , resolve( 'src/store' ) )
         .set( '@assets' , resolve( 'src/assets' ) )
         .set( '@plugins' , resolve( 'src/plugins' ) )
+
+        const types = [ 'vue-modules' , 'vue' , 'normal-modules' , 'normal' ]
+
+        types.forEach( type => addStyleResource( config.module.rule( 'less' ).oneOf( type ) ) )
     } ,
+
     devServer : {
         //配置端口
         port : 8338
