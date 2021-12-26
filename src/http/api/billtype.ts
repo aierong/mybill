538a397cb8502aa9caedf5fa183ffa7c068bc48b
aierong <aierong@126.com>
2021-12-26 17:15:37 +0800

    home

--- src/http/api/billtype.ts
+++ src/http/api/billtype.ts
@@ -7,13 +7,22 @@
 
  */
 // import axios from 'axios'
-import axios from '@/http/http.ts';
-import { IResult , IBillTypesListResult } from '@/types/index.ts'
+import axios from '../http';
+import { IResult , IBillTypesListResult , IAxiosResult } from '@/types/index.ts'
+import { IBillType } from "@comp/types";
 
 const prefix = '/billtypes';
 
 export function getlist ( isout : boolean , isrefresh : boolean = true ) {
     return axios.get<IBillTypesListResult>( `${ prefix }/getallbilltype/${ isout }/${ isrefresh }` );
+}
+
+export function getlistnew ( isout : boolean , isrefresh : boolean = true ) {
+    return axios.get<IAxiosResult<IBillType[]>>( `${ prefix }/getallbilltype/${ isout }/${ isrefresh }` );
+}
+
+export function getlistnew2 ( isout : boolean , isrefresh : boolean = true ) : Promise<IAxiosResult<IBillType[]>> {
+    return axios.get( `${ prefix }/getallbilltype/${ isout }/${ isrefresh }` );
 }
 
 export function add ( isout : boolean , typename : string = '' ) {
