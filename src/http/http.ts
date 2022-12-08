181bd4ca34c56f56acca05a55f85a61a4881b3e7
aierong <aierong@126.com>
2022-12-08 11:41:22 +0800

    http

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,11 +1,9 @@
 /*
-
  作者: chenghao
  Date: 2021/4/27
  Time: 16:55
  功能: TypeScript脚本
-
- */
+*/
 
 import axios , { AxiosRequestConfig , AxiosResponse } from 'axios';
 import { tokenname } from '@common/constant'
