2e2b2556b30b51215bb3b818796236f60fecc462
aierong <aierong@126.com>
2023-01-04 19:11:53 +0800

    Update FluentValidationServiceExtensions.cs

--- service/billservice/billservice/Extensions/FluentValidationServiceExtensions.cs
+++ service/billservice/billservice/Extensions/FluentValidationServiceExtensions.cs
@@ -21,9 +21,17 @@ namespace billservice.Extensions
         /// <returns></returns>
         public static IServiceCollection AddFluentValidationService ( this IServiceCollection services , IConfiguration configuration )
         {
-            
+
             //services.AddMvc().AddFluentValidation();
-            services.AddFluentValidationAutoValidation();  //新版本注册
+            services.AddFluentValidationAutoValidation( config =>
+            {
+                //执行FluentValidation后，任何其他验证器提供程序也将有机会执行。这意味着您可以将FluentValidation自动验证与DataAnnotations属性（或任何其他ASP.NET ModelValidatorProvider实现）混合使用。
+                //以便FluentValidation是唯一执行的验证库
+                config.DisableDataAnnotationsValidation = true;
+
+                //config.ImplicitlyValidateChildProperties = true;
+                //config.ImplicitlyValidateRootCollectionElements = true;
+            } );  //新版本注册
 
 
             services.AddTransient<IValidator<UserAddDto> , UserAddDtoValidator>();
