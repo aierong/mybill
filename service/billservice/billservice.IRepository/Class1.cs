ceda8e1826df4bb83547fa16f736705fd6651a50
aierong <aierong@126.com>
2023-01-26 16:13:05 +0800

    commpany

---
+++ service/billservice/billservice.IRepository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace billservice.IRepository
+{
+    public class Class1
+    {
+    }
+}
---
+++ service/billservice/billservice.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace billservice.Repository
+{
+    public class Class1
+    {
+    }
+}
