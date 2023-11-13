7bf2e238827c7a50f554092d0c39f04ad1b69a14
aierong <aierong@126.com>
2023-11-13 21:18:42 +0800

    add

--- service/billservice/billservice/Extensions/ServiceExtensions.cs
+++ service/billservice/billservice/Extensions/ServiceExtensions.cs
@@ -99,5 +99,39 @@ namespace billservice.Extensions
             return services;
         }
 
+
+
+        /// <summary>
+        /// 跨域的相关服务注册
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCorsService ( this IServiceCollection services , IConfiguration configuration )
+        {
+            CorsConfigData _CorsConfigData = configuration.GetSection( "CorsInfo" ).Get<CorsConfigData>();
+
+            //定义名称
+            string PolicyName = _CorsConfigData.name;
+            string[] urls = _CorsConfigData.urls.ToArray();
+
+            //添加cors 服务 配置跨域处理            
+            services.AddCors( options => options.AddPolicy( PolicyName ,
+             builder =>
+             {
+
+                 builder.WithOrigins( urls )
+                                 .AllowAnyMethod()
+                                 .AllowAnyHeader()
+                                 .AllowCredentials();
+
+             } ) );
+
+            return services;
+        }
+
+
+
+
     }
 }
