1dccb364c8972d715281d424c505360c8f1fbd08
aierong <aierong@126.com>
2023-11-13 21:17:15 +0800

    add

--- service/billservice/billservice/Extensions/ServiceExtensions.cs
+++ service/billservice/billservice/Extensions/ServiceExtensions.cs
@@ -5,6 +5,10 @@ using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using billservice.Helpers.ConfigData;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace billservice.Extensions
 {
@@ -50,6 +54,40 @@ namespace billservice.Extensions
             services.AddTransient<IValidator<UserLoginDto> , UserLoginDtoValidator>();
             services.AddTransient<IValidator<BillTypeDto> , BillTypeDtoValidator>();
             services.AddTransient<IValidator<BillDto> , BillDtoValidator>();
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddTokenService ( this IServiceCollection services , IConfiguration configuration )
+        {
+            const string sectionname = "TokenInfo";
+
+            services.Configure<TokenConfigData>( configuration.GetSection( sectionname ) );
+
+
+            //把配置文件中，配置信息加载回来
+            TokenConfigData _tokenconfigdata = configuration.GetSection( sectionname ).Get<TokenConfigData>();
+
+
+            // 注册JwtBearer服务
+            services.AddAuthentication( JwtBearerDefaults.AuthenticationScheme ).AddJwtBearer( options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+
+                    ValidateIssuer = true ,  //是否验证Issuer
+                    ValidateAudience = true ,//是否验证Audience
+                    ValidateLifetime = true ,//是否验证失效时间
+                    ValidateIssuerSigningKey = true ,//是否验证SecurityKey
+
+                    ValidAudience = _tokenconfigdata.audience , // "http://localhost:5001" ,//Audience
+                    ValidIssuer = _tokenconfigdata.issuer , // "http://localhost:5000" ,//Issuer，这两项和前面签发jwt的设置一致
+                                                            //IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( "1234567890123456key" ) )//拿到SecurityKey
+                    IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( _tokenconfigdata.key ) )//拿到SecurityKey
+
+                };
+            } );
 
             return services;
         }
--- service/billservice/billservice/Extensions/TokenServiceExtensions.cs
+++ service/billservice/billservice/Extensions/TokenServiceExtensions.cs
@@ -13,38 +13,38 @@ namespace billservice.Extensions
 {
     public static class TokenServiceExtensions
     {
-        public static IServiceCollection AddTokenService ( this IServiceCollection services , IConfiguration configuration )
-        {
-            const string sectionname = "TokenInfo";
+        //public static IServiceCollection AddTokenService ( this IServiceCollection services , IConfiguration configuration )
+        //{
+        //    const string sectionname = "TokenInfo";
 
-            services.Configure<TokenConfigData>( configuration.GetSection( sectionname ) );
-
-
-            //把配置文件中，配置信息加载回来
-            TokenConfigData _tokenconfigdata = configuration.GetSection( sectionname ).Get<TokenConfigData>();
+        //    services.Configure<TokenConfigData>( configuration.GetSection( sectionname ) );
 
 
-            // 注册JwtBearer服务
-            services.AddAuthentication( JwtBearerDefaults.AuthenticationScheme ).AddJwtBearer( options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
+        //    //把配置文件中，配置信息加载回来
+        //    TokenConfigData _tokenconfigdata = configuration.GetSection( sectionname ).Get<TokenConfigData>();
 
-                    ValidateIssuer = true ,  //是否验证Issuer
-                    ValidateAudience = true ,//是否验证Audience
-                    ValidateLifetime = true ,//是否验证失效时间
-                    ValidateIssuerSigningKey = true ,//是否验证SecurityKey
 
-                    ValidAudience = _tokenconfigdata.audience , // "http://localhost:5001" ,//Audience
-                    ValidIssuer = _tokenconfigdata.issuer , // "http://localhost:5000" ,//Issuer，这两项和前面签发jwt的设置一致
-                                                            //IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( "1234567890123456key" ) )//拿到SecurityKey
-                    IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( _tokenconfigdata.key ) )//拿到SecurityKey
+        //    // 注册JwtBearer服务
+        //    services.AddAuthentication( JwtBearerDefaults.AuthenticationScheme ).AddJwtBearer( options =>
+        //    {
+        //        options.TokenValidationParameters = new TokenValidationParameters
+        //        {
 
-                };
-            } );
+        //            ValidateIssuer = true ,  //是否验证Issuer
+        //            ValidateAudience = true ,//是否验证Audience
+        //            ValidateLifetime = true ,//是否验证失效时间
+        //            ValidateIssuerSigningKey = true ,//是否验证SecurityKey
 
-            return services;
-        }
+        //            ValidAudience = _tokenconfigdata.audience , // "http://localhost:5001" ,//Audience
+        //            ValidIssuer = _tokenconfigdata.issuer , // "http://localhost:5000" ,//Issuer，这两项和前面签发jwt的设置一致
+        //                                                    //IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( "1234567890123456key" ) )//拿到SecurityKey
+        //            IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( _tokenconfigdata.key ) )//拿到SecurityKey
+
+        //        };
+        //    } );
+
+        //    return services;
+        //}
 
 
     }
