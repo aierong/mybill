2612e558c4f668807bfcc20e087608116abbbac5
aierong <aierong@126.com>
2024-01-04 20:06:30 +0800

    up

--- service/billservice/billservice.models/Dto/BillDto.cs
+++ service/billservice/billservice.models/Dto/BillDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace billservice.models.Dto
+﻿namespace billservice.models.Dto
 {
     public class BillDto
     {
--- service/billservice/billservice.models/Dto/UserLoginDto.cs
+++ service/billservice/billservice.models/Dto/UserLoginDto.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace billservice.models.Dto
+﻿namespace billservice.models.Dto
 {
     public class UserLoginDto
     {
