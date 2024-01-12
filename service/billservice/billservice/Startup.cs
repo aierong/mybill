98945d1ae48631809d961d343a83b1362de3f973
aierong <aierong@126.com>
2024-01-12 08:29:12 +0800

    up

--- service/billservice/billservice/Startup.cs
+++ service/billservice/billservice/Startup.cs
@@ -1,21 +1,12 @@
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Threading.Tasks;
 using billservice.Extensions;
-using billservice.Helpers;
-using billservice.Helpers.ConfigData;
 using billservice.Helpers.Result;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using EasyCaching.InMemory;
 
 
 
