62ad2002f406012fc00e20429225bf1779158cf7
aierong <aierong@126.com>
2024-01-28 10:32:18 +0800

    up

--- service/billservice/Utils/DataRowHelper.cs
+++ service/billservice/Utils/DataRowHelper.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
 
 namespace Utils
 {
+    /// <summary>
+    /// 帮助类
+    /// </summary>
     public class DataRowHelper
     {
 
