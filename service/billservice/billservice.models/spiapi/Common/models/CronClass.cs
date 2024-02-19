cc7e68aeeb72287766be6721c3d40ef0af787962
aierong <aierong@126.com>
2024-02-19 15:28:11 +0800

    up

---
+++ service/billservice/billservice.models/spiapi/Common/models/CronClass.cs
@@ -0,0 +1,17 @@
+﻿namespace billservice.models.spiapi.Common.models
+{
+    public class CronClass
+    {
+        public string cron
+        {
+            get; set;
+        }
+
+
+
+        public string remarks
+        {
+            get; set;
+        }
+    }
+}
