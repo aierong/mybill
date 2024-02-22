46062a60623657b11d71ecde5b1e10380bfdd896
aierong <aierong@126.com>
2024-02-22 08:44:10 +0800

    up

---
+++ service/billservice/billservice.IRepository/zw/ISpiDataRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+
+namespace spiapi.irepository
+{
+    public interface ISpiDataRepository
+    {
+
+        //SpiResult CheckAndRun ( Spi _model , SpiDl dl , SpiParameters _SpiParameters );
+
+
+       
+
+    }
+}
