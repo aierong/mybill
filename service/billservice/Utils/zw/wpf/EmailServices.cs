ad229db8d6e7295b013030ea4b2628b6eab7bf36
aierong <aierong@126.com>
2025-02-10 09:37:09 +0800

    update

---
+++ service/billservice/Utils/zw/wpf/EmailServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+ 
+
+namespace spiapi.services
+{
+    public class EmailServices  
+    {
+        
+
+
+
+        public EmailServices (   )
+        {
+            
+        }
+
+
+
+       
+    }
+}
