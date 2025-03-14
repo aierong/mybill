8a8325da2667b0e2e5b732fe39232e6cbb7c5369
aierong <aierong@126.com>
2025-03-14 13:16:42 +0800

    up

---
+++ service/billservice/Utils/zw/demo/ConfigData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPIQC.Models
+{
+    public class ConfigData
+    {
+        public string RefreshTime
+        {
+            get; set;
+        }
+    }
+}
---
+++ service/billservice/billservice.services/zw/api/IEmailRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+
+
+
+namespace spiapi.irepository
+{
+    public interface IEmailRepository
+    {
+        
+
+        Task<DataTable> GetDataListAsync ( string sql );
+    }
+}
